084863b34282350e059703ac851a81265ad61644
Lenovo <vishalthapa.cs@gmail.com>
2017-12-16 13:51:30 +0545

    test commit

--- app/src/main/java/com/example/lenovo/ehospital/HospitalActivity.java
+++ app/src/main/java/com/example/lenovo/ehospital/HospitalActivity.java
@@ -27,6 +27,7 @@ public class HospitalActivity extends Fragment {
     AQuery aQuery;
 
 //    String fetchUrl = "http://10.0.2.2/ehospital/hospital/select.php";
+
     String fetchUrl = "http://192.168.100.20/ehospital/hospital/select.php";
 
 
